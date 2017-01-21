dd3a9f4768b86ede922f2000a68e0b6e229425a2
Zeon Trevor Fernando <zeon.trevor@gmail.com>
2017-01-21 13:15:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var counter = 0;
+  function addBump(){
+    return ++counter
+  }
+  function getBumps(){
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType){
+  return (deadlyDevice) => {
+    "animalType": animalType,
+    "deadlyDevice":deadlyDevice
+  }
+}
+
+var sharkCreator = createAnimal("Shark")
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+var sharkWithFrickinCanon = sharkCreator("Canon")
