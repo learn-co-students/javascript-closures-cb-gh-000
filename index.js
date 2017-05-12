4ec4acf97278f5755159ee108ec0f756ece78b9c
Belal Mohammed <belal.mohamed@acu.edu.eg>
2017-05-12 16:24:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+
+
+function Greetings(greetingWord){
+  function helloWorld(){
+    if(greetingWord === 'hello world'){
+      console.log('NO! you\'re not allowed to greet the world');
+    } else {
+      console.log("Okay XD");
+    }
+  }
+
+  function mMounir(){
+    return 'Mohammed Mounir';
+  }
+
+  return {
+    helloWorld,
+    mMounir
+  }
+  
+}
