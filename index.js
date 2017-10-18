7035b9f6aa70cad97cc9f987be15d4862462928c
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-18 13:59:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+function fatBastard(meal) {
+  function whatsForDinner() { // whatsForDinner() is the inner function, a closure
+    if (meal === 'Mini-Me') { // whatsForDinner() uses argument provided to the parent function
+      console.log('The wee man is in my belly!');
+    } else {
+      console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+
+  return whatsForDinner;
+}
+
+function raspyDoorGuy() {
+  const password = 'yarr'; // password is a local variable created by raspyDoorGuy()
+
+  function givePassword(givenPassword) { // givePassword() is an inner function, a closure
+    if (givenPassword === password) { // givePassword() uses variable declared in the parent function
+      console.log('Ye may enter.');
+    } else {
+      console.log('Begone, landlubber!');
+    }
+  }
+
+  function willBreakPrinciples(bribeAmount) { // willBreakPrinciples() is the private method
+    return bribeAmount >= 50;
+  }
+
+  function bribe(amount) { // bribe() is an inner function, a closure
+    if (willBreakPrinciples(amount)) { // bribe() uses private method created in the parent function
+      return password; // bribe() uses variable declared in the parent function
+    } else {
+      console.log("Pssht. That won't work.");
+    }
+  }
+
+  return {
+    givePassword,
+    bribe
+  };
+}
