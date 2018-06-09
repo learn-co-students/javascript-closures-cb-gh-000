efb238f708baa49e72515a5f6f8f77187fee98cd
Ruben Vargas <rubenbustos33@gmail.com>
2018-06-09 20:52:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const app = "I don't do much."
+function fatBastard(meal) {
+  function whatsForDinner() { // whatsForDinner() is the inner function, a closure
+    if (meal === 'Mini-Me') { // whatsForDinner() uses argument provided to the parent function
+      console.log('The wee man is in my belly!');
+    } else {
+      console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+
+  return whatsForDinner;
+}
+
+// const whatsForDinner = fatBastard('Kobe beef');
+// whatsForDinner(); // prints 'I'm eatin' a bit of Kobe beef! Burp.'
+// const whatsInHisTummy = fatBastard('Mini-Me');
+// whatsInHisTummy(); // prints 'The wee man is in my belly!'
