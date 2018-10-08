f76cf4dd62c2a5872bf72717fdda5c4d2f6718b3
Anandha Sudhan <rasudhan@gmail.com>
2018-10-08 15:05:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-const app = "I don't do much."
+function fatBastard(meal) {
+  function whatsForDinner() {
+    if (!meal) {
+      console.log('My belly is empty. Woe is me.');
+    } else if (meal === 'Mini-Me') {
+      console.log('The wee man is in my belly!');
+    } else {
+      console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+
+  function digest() {
+    meal = undefined;
+  }
+
+  return {
+    whatsForDinner,
+    digest
+  };
+}
+
+const { whatsForDinner, digest } = fatBastard('ribeye');
+whatsForDinner(); // prints 'I'm eatin' a bit of ribeye! Burp.'
+digest();
+whatsForDinner(); // prints 'My belly is empty. Woe is me.'
 
 function raspyDoorGuy() {
-  const password = 'yarr'; // password is a local variable created by raspyDoorGuy()
+  const password = 'yarr';
 
-  function givePassword(givenPassword) { // givePassword() is an inner function, a closure
-    if (givenPassword === password) { // givePassword() uses variable declared in the parent function
+  function givePassword(givenPassword) {
+    if (givenPassword === password) {
       console.log('Ye may enter.');
     } else {
       console.log('Begone, landlubber!');
     }
   }
 
-  function willBreakPrinciples(bribeAmount) { // willBreakPrinciples() is the private method
+  function willBreakPrinciples(bribeAmount) {
     return bribeAmount >= 50;
   }
 
-  function bribe(amount) { // bribe() is an inner function, a closure
-    if (willBreakPrinciples(amount)) { // bribe() uses private method created in the parent function
-      return password; // bribe() uses variable declared in the parent function
+  function bribe(amount) {
+    if (willBreakPrinciples(amount)) {
+      return password;
     } else {
       console.log("Pssht. That won't work.");
     }
@@ -31,3 +55,4 @@ function raspyDoorGuy() {
 
 const { bribe } = raspyDoorGuy();
 bribe(80); // returns 'yarr'
+console.log(bribe(80));
