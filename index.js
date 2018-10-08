7d54aba18217cc1a73d5efac81003ca1f2ada3a6
Anandha Sudhan <rasudhan@gmail.com>
2018-10-08 15:08:42 +0000

    Done.

--- index.js
+++ index.js
@@ -24,24 +24,25 @@ whatsForDinner(); // prints 'I'm eatin' a bit of ribeye! Burp.'
 digest();
 whatsForDinner(); // prints 'My belly is empty. Woe is me.'
 
-function raspyDoorGuy() {
-  const password = 'yarr';
 
-  function givePassword(givenPassword) {
-    if (givenPassword === password) {
+function raspyDoorGuy() {
+  const password = 'yarr'; // password is a local variable created by raspyDoorGuy()
+
+  function givePassword(givenPassword) { // givePassword() is an inner function, a closure
+    if (givenPassword === password) { // givePassword() uses variable declared in the parent function
       console.log('Ye may enter.');
     } else {
       console.log('Begone, landlubber!');
     }
   }
 
-  function willBreakPrinciples(bribeAmount) {
+  function willBreakPrinciples(bribeAmount) { // willBreakPrinciples() is the private method
     return bribeAmount >= 50;
   }
 
-  function bribe(amount) {
-    if (willBreakPrinciples(amount)) {
-      return password;
+  function bribe(amount) { // bribe() is an inner function, a closure
+    if (willBreakPrinciples(amount)) { // bribe() uses private method created in the parent function
+      return password; // bribe() uses variable declared in the parent function
     } else {
       console.log("Pssht. That won't work.");
     }
@@ -55,4 +56,3 @@ function raspyDoorGuy() {
 
 const { bribe } = raspyDoorGuy();
 bribe(80); // returns 'yarr'
-console.log(bribe(80));
