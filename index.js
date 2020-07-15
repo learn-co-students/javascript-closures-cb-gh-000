a4da8188cf76be622b8865f2050496a810400233
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-07-15 10:30:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+function raspyDoorGuy() {
+  const password = 'yarr'; // password is a local variable created by raspyDoorGuy()
+
+  function givePassword(givenPassword) { // givePassword() is an inner function, a closure
+    if (givenPassword === password) { // givePassword() uses variable declared in the parent function
+      console.log('Ye may enter.');
+    } else {
+      console.log('Begone, landlubber!');
+    }
+  }
+
+  function willBreakPrinciples(bribeAmount) { // willBreakPrinciples() is the private method
+    return bribeAmount >= 50;
+  }
+
+  function bribe(amount) { // bribe() is an inner function, a closure
+    if (willBreakPrinciples(amount)) { // bribe() uses private method created in the parent function
+      return password; // bribe() uses variable declared in the parent function
+    } else {
+      console.log("Pssht. That won't work.");
+    }
+  }
+
+  return {
+    givePassword,
+    bribe
+  };
+}
